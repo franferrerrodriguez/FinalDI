b1ff16c8634a10a64ff1b0c01f914b33fc6b0ed5
Francisco J. Ferrer <franferrerrodriguez@gmail.com>
2020-03-15 23:00:27 +0100

    Se avanzan gráficos

--- PresentacionWpf/Estadisticas/FichaEstadisticasPedidosDiaMes.xaml.cs
+++ PresentacionWpf/Estadisticas/FichaEstadisticasPedidosDiaMes.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Capa_entidades;
 using CapaNegocio;
+using LiveCharts;
+using LiveCharts.Wpf;
 using System;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,6 +18,10 @@ namespace PresentacionWpf
         public Modos modo;
         private MainWindow mainWindow;
         private UserControl userControlParent;
+        private string[] meses;
+        private string[] anos;
+        private int mes;
+        private int ano;
 
         public FichaEstadisticasPedidosDiaMes(Modos modo, Window windowParent = null, UserControl userControlParent = null)
         {
@@ -23,10 +29,45 @@ namespace PresentacionWpf
             this.modo = modo;
             mainWindow = (MainWindow)windowParent;
             this.userControlParent = userControlParent;
-            UtilsControl.SetTitulo(modo, labelTitle, "pedidos por día del mes");
+            UtilsControl.SetTitulo(modo, labelTitle, "pedidos por días del mes");
             mainWindow.SetStatusException();
+            
+            // Seteamos los meses
+            meses = GetMeses();
+            comboBoxPedidosMes.ItemsSource = meses;
+            comboBoxPedidosMes.SelectedIndex = DateTime.Now.Month - 1;
+            mes = comboBoxPedidosMes.SelectedIndex + 1;
+
+            // Seteamos los años
+            anos = GetAnos();
+            comboBoxPedidosAnyo.ItemsSource = anos;
+            comboBoxPedidosAnyo.SelectedIndex = 0;
+            ano = Convert.ToInt32(comboBoxPedidosAnyo.SelectedItem.ToString());
+
+            // GRÁFICO BARCHART
+            RefreshChart();
         }
-        
+
+        private void RefreshChart()
+        {
+            SeriesCollection serie = new SeriesCollection();
+            g2_eje_x.MaxValue = 5;
+
+            // Creo una serie con dos COLUMNAS
+            serie.Add(new ColumnSeries
+            {
+                Title = "2017",
+                Values = new ChartValues<double> { 10, 50, 39, 10, 100 }
+            });
+            serie.Add(new ColumnSeries
+            {
+                Title = "2018",
+                Values = new ChartValues<double> { 23, 44, 50, 5, 100 }
+            });
+
+            g2_eje_x.Labels = new[] { "Maria", "Juan", "Pedro", "Jairo", "Mario" };
+            graficoPedidosDiaMes.Series = serie;
+        }
 
         private void BtnCancelar_Click(object sender, RoutedEventArgs e)
         {
--- PresentacionWpf/Estadisticas/FichaEstadisticasPedidosTipo.xaml.cs
+++ PresentacionWpf/Estadisticas/FichaEstadisticasPedidosTipo.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using Capa_entidades;
 using CapaNegocio;
+using LiveCharts;
+using LiveCharts.Wpf;
 using System;
 using System.Windows;
 using System.Windows.Controls;
-using Utils;
 using static Utils.Utilities;
 
 namespace PresentacionWpf
@@ -16,6 +17,7 @@ namespace PresentacionWpf
         public Modos modo;
         private MainWindow mainWindow;
         private UserControl userControlParent;
+        private ProductosNegocio productosNegocio;
 
         public FichaEstadisticasPedidosTipo(Modos modo, Window windowParent = null, UserControl userControlParent = null)
         {
@@ -25,6 +27,49 @@ namespace PresentacionWpf
             this.userControlParent = userControlParent;
             UtilsControl.SetTitulo(modo, labelTitle, "pedidos por tipo");
             mainWindow.SetStatusException();
+
+            productosNegocio = new ProductosNegocio();
+
+            try
+            {
+                comboBoxPedidosTipo.ItemsSource = productosNegocio.LeerTiposArticulos();
+                comboBoxPedidosTipo.SelectedIndex = 0;
+            }
+            catch (Exception e)
+            {
+                mainWindow.SetStatusException(e);
+            }
+
+            // GRÁFICO PIECHART
+            RefreshChart();
+        }
+
+        private void RefreshChart()
+        {
+            SeriesCollection serie = new SeriesCollection();
+            serie.Add(new PieSeries
+            {
+                Title = "Juan",
+                Values = new ChartValues<double> { 10 },
+                Stroke = System.Windows.Media.Brushes.HotPink,
+                Fill = System.Windows.Media.Brushes.HotPink
+            });
+            serie.Add(new PieSeries
+            {
+                Title = "Mario",
+                Values = new ChartValues<double> { 60 },
+                Stroke = System.Windows.Media.Brushes.Green,
+                Fill = System.Windows.Media.Brushes.Green
+            });
+            serie.Add(new PieSeries
+            {
+                Title = "Ana",
+                Values = new ChartValues<double> { 30 },
+                Stroke = System.Windows.Media.Brushes.Aquamarine,
+                Fill = System.Windows.Media.Brushes.Aquamarine
+            });
+
+            graficoPedidosTipo.Series = serie;
         }
 
         private void BtnCancelar_Click(object sender, RoutedEventArgs e)
